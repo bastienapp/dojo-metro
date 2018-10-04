9ad9e3d213526cf5e5344ad7d8c02ac02f3175cc
Bastien Krettly <bastien_krettly@wilder.school>
2018-10-04 12:01:16 +0200

    Update Metro.java

--- solution/Metro.java
+++ solution/Metro.java
@@ -1,10 +1,13 @@
 class Metro {
 
-    public static int count(int[] metro, int max, int waiting) {
-        int start = waiting;
+    public static int count (int metro [], int max, int waiting) {
+        int nbEntree = 0;
         for (int i = 0; i < metro.length && waiting >= 0; i++) {
-            waiting -= (max - metro[i]);
+            int cap = max - metro[i]; // places restantes
+            cap = Math.min(cap, waiting); // min entre places restantes et file d'attente
+            nbEntree += cap; // augmente le nombre de personne entrées dans le métro
+            waiting -= cap; // on diminue le nombre de personne dans la file d'attente
         }
-        return waiting >= 0 ? start - waiting : start;
+        return nbEntree;
     }
 }
